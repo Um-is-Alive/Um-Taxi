38b11ab8a8f6847c1a55373f4aa0ceda7bd0031a
이순형 <tnsgud9@naver.com>
2020-04-21 21:54:48 +0900

    MainActivity Update

--- app/src/main/java/com/nextop/project/um_taxi/MainActivity.java
+++ app/src/main/java/com/nextop/project/um_taxi/MainActivity.java
@@ -2,13 +2,31 @@ package com.nextop.project.um_taxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.nextop.project.um_taxi", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.i("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (Exception e) {
+            Log.e("MainActivity", e.getStackTrace().toString());
+        }
     }
 }
